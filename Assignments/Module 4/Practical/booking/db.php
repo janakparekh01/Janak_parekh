dfd8ad3edc0c81bc78b8866f9c5b3ac0665e1670
janakparekh01 <132338938+janakparekh01@users.noreply.github.com>
2023-12-05 14:12:33 +0530

    Add files via upload

---
+++ Assignments/Module 4/Practical/booking/db.php
@@ -0,0 +1,37 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "bookings";
+
+// Create connection
+$connection = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($connection->connect_error) {
+    die("Connection failed: " . $connection->connect_error);
+}
+//connected to database and created a variable connection
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $fullName = $_POST['fullName'];
+    $checkInDate = $_POST['checkInDate'];
+    $checkOutDate = $_POST['checkOutDate'];
+    $bookingType = $_POST['bookingType'];
+
+    // Check if room is available based on the booking type and date range
+    // Replace 'select available rooms' with your actual SQL query
+    $query = "SELECT available rooms WHERE check_in_date <= '$checkInDate' AND check_out_date >= '$checkOutDate'";
+    $result = mysqli_query($connection, $query);
+
+        // Insert the booking details into the database
+        // Replace 'insert booking details' with your actual SQL query
+        $query = "INSERT INTO bookings (full_name, check_in_date, check_out_date, booking_type) VALUES ('$fullName', '$checkInDate', '$checkOutDate', '$bookingType')";
+        mysqli_query($connection, $query);
+
+        echo "Room booked successfully!";
+   
+}
+
+?>
